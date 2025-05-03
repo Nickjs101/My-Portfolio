61e1bc4aa8da2e63b8711aba552f109f6e0c3534
Nick Joshua Posadas Montemayor <nickjoshuamontemayor0121@gmail.com>
2025-05-03 06:20:52 +0000

    Revert "project:AutomatedProjectPageGenerator,folderID:1V4aB4-9AMY2ijN023KjdH4bweABzv2go"
    This reverts commit 9552e96ab2d9678189ff12eda936acf9d8f4485a.

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -5,7 +5,6 @@ import vul from '../../assets/Linux/Picture2.png';
 import Sql from '../../assets/Linux/Picture1.png';
 import YouTubeShortsAutomationCover from '../../assets/Works/youtubeshortsautomationcover.png';
 import AppointmentSetterAgentCover from '../../assets/Works/AppointmentSetterAgentCover.png';
-import pagegeneratorCover from '../../assets/Works/pagegeneratorCover.png';
 
 import AppProject1 from '../../components/subcomponents/AppProject1';
 import AppProject2 from '../../components/subcomponents/AppProject2';
@@ -15,33 +14,8 @@ import Project3 from '../../components/subcomponents/Project3';
 import Project4 from '../../components/subcomponents/Project4';
 import YouTubeShortsAutomation from '../../components/subcomponents/YoutubeShortsAutomation';
 import AppointmentSetterAIAgentSystem from '../../components/subcomponents/AppointmentSetterAIAgentSystem.jsx';
-import AutomatedProjectPageGenerator from '../../components/subcomponents/AutomatedProjectPageGenerator.jsx';
 
 export const Latest = [
-  {
-    id: 6,
-    title: 'Automated Project Page Generator',
-    category: 'Software Development',
-    description:
-      'An AI-driven tool that automates the creation of project showcase pages to streamline portfolio development processes.',
-    type: 'Software Development',
-    image: pagegeneratorCover,
-    skills: [
-      'Git',
-      'GPT Language Models',
-      'GPT Image Model',
-      'Google Drive',
-      'Bash',
-      'Telegram',
-      'Netlify',
-      'n8n',
-    ],
-    component: <AutomatedProjectPageGenerator />,
-    links: {
-      github: null,
-      live: null,
-    },
-  },
   {
     id: 5,
     title: 'AI RAG & Appointment System',
@@ -120,29 +94,6 @@ export const Latest = [
 ];
 
 export const SoftwareDev = [
-  {
-    id: 6,
-    title: 'Automated Project Page Generator',
-    category: 'Software Development',
-    description:
-      'An AI-driven tool that automates the creation of project showcase pages to streamline portfolio development processes.',
-    image: pagegeneratorCover,
-    skills: [
-      'Git',
-      'GPT Language Models',
-      'GPT Image Model',
-      'Google Drive',
-      'Bash',
-      'Telegram',
-      'Netlify',
-      'n8n',
-    ],
-    component: <AutomatedProjectPageGenerator />,
-    links: {
-      github: null,
-      live: null,
-    },
-  },
   {
     title: "Kody's Adventure",
     category: 'Software Development',
