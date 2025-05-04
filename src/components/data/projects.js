964c0db85fb146a9cc52531eb9397f7903ef4bd6
Nick Joshua Posadas Montemayor <nickjoshuamontemayor0121@gmail.com>
2025-05-04 14:39:14 +0000

    Revert "project:AutomatedProjectPageGenerator,folderID:1V4aB4-9AMY2ijN023KjdH4bweABzv2go"
    This reverts commit 4a57c8e5b3e55f4f12731d3295f89286c597b857.

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -5,7 +5,6 @@ import vul from '../../assets/Linux/Picture2.png';
 import Sql from '../../assets/Linux/Picture1.png';
 import YouTubeShortsAutomationCover from '../../assets/Works/youtubeshortsautomationcover.png';
 import AppointmentSetterAgentCover from '../../assets/Works/AppointmentSetterAgentCover.png';
-import pagegeneratorCover from '../../assets/Works/pagegeneratorCover.png';
 
 import AppProject1 from '../../components/subcomponents/AppProject1';
 import AppProject2 from '../../components/subcomponents/AppProject2';
@@ -15,24 +14,8 @@ import Project3 from '../../components/subcomponents/Project3';
 import Project4 from '../../components/subcomponents/Project4';
 import YouTubeShortsAutomation from '../../components/subcomponents/YoutubeShortsAutomation';
 import AppointmentSetterAIAgentSystem from '../../components/subcomponents/AppointmentSetterAIAgentSystem.jsx';
-import AutomatedProjectPageGenerator from '../../components/subcomponents/AutomatedProjectPageGenerator.jsx';
 
 export const Latest = [
-  {
-    id: 6,
-    title: 'Automated Project Page Generator',
-    category: 'Automation',
-    description:
-      'A tool that automates the creation and deployment of project showcase pages using AI and workflows to enhance portfolio efficiency.',
-    type: 'Automation',
-    image: pagegeneratorCover,
-    skills: ['Git', 'n8n', 'GPT Language Models', 'Google Drive', 'Bash', 'Telegram', 'Netlify'],
-    component: <AutomatedProjectPageGenerator />,
-    links: {
-      github: null,
-      live: null,
-    },
-  },
   {
     id: 5,
     title: 'AI RAG & Appointment System',
@@ -193,21 +176,6 @@ export const Cybersecurity = [
 ];
 
 export const Automation = [
-  {
-    id: 6,
-    title: 'Automated Project Page Generator',
-    category: 'Automation',
-    description:
-      'A tool that automates the creation and deployment of project showcase pages using AI and workflows to enhance portfolio efficiency.',
-    type: 'Automation',
-    image: pagegeneratorCover,
-    skills: ['Git', 'n8n', 'GPT Language Models', 'Google Drive', 'Bash', 'Telegram', 'Netlify'],
-    component: <AutomatedProjectPageGenerator />,
-    links: {
-      github: null,
-      live: null,
-    },
-  },
   {
     id: 5,
     title: 'AI RAG & Appointment System',
